0ad6cf260cfe9e2376a9cc3c7ffff519f7a9b8e9
watson <watson-y@outlook.com>
2019-07-08 23:37:12 +0800

    尝试修改vant主题

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import Vue from 'vue';
+import Vant from 'vant';
+import { Lazyload } from 'vant';
+import { Dialog } from 'vant';
 import App from './App.vue';
 import router from './router/index';
 import store from './store/index';
+import 'vant/lib/index.less';
+import '../static/style/youzan/style.less';
 
 Vue.config.productionTip = false;
 
+// 使用有赞UI组件库
+Vue.use(Vant);
+Vue.use(Lazyload);
+
+Vue.prototype.Dialog = Dialog;
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <!-- <van-swipe :autoplay="3000">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img v-lazy="image" />
+      </van-swipe-item>
+    </van-swipe> -->
+    <Hello /> 
   </div>
 </template>
 
@@ -15,5 +21,14 @@ import Hello from '@/components/Hello.vue';
     Hello,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public data() {
+    return {
+      images: [
+        'https://img.yzcdn.cn/vant/apple-1.jpg',
+        'https://img.yzcdn.cn/vant/apple-2.jpg',
+      ],
+    };
+  }
+}
 </script>
