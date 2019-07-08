4a87e89c42f4b24f2b9a56df65788c9c9b00c48f
watson <watson-y@outlook.com>
2019-07-08 22:19:06 +0800

    初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Hello from '@/components/Hello.vue';
+@Component({
+  components: {
+    HelloWorld,
+    Hello,
+  },
+})
+export default class Home extends Vue {}
+</script>
